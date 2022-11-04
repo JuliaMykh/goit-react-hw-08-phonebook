b2fa10e8e02d48c4d18ef3e66a3253f2e40121fb
Mykhailenko <juliam4433@gmail.com>
2022-11-04 13:50:43 +0100

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -4,7 +4,7 @@ import Button from '@mui/material/Button';
 
 import { getName } from '../../redux/auth/authSelectors';
 import authOperetions from 'redux/auth/authOperations';
-import { UserMenuBox, UserMenuText, UserMenuName, UserMenuBtn } from './UserMenu.styled';
+import { UserMenuBox, UserMenuText, UserMenuName } from './UserMenu.styled';
 
 export const UserMenu = () => {
 
